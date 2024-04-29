ef00e53993e3748ce6de0861fbb3d8d509908a33
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-04-29 11:12:14 +0600

    initial commit

---
+++ test-data-json.js
@@ -0,0 +1,39 @@
+let test = [    //array
+    {    //array[0]
+    "id": 1,
+    "first_name": "Jeanette",
+    "last_name": "Penddreth",
+    "email": "jpenddreth0@census.gov",
+    "gender": "Female",
+    "ip_address": "26.58.193.2"
+    }, 
+    {    //array[1]
+    "id": 2,
+    "first_name": "Giavani",
+    "last_name": "Frediani",
+    "email": "gfrediani1@senate.gov",
+    "gender": "Male",
+    "ip_address": "229.179.4.212"
+    }, 
+    {    //array[2]
+    "id": 3,
+    "first_name": "Noell",
+    "last_name": "Bea",
+    "email": "nbea2@imageshack.us",
+    "gender": "Female",
+    "ip_address": "180.66.162.255"
+    }, 
+    {    //array[3]
+    "id": 4,
+    "first_name": "Willard",
+    "last_name": "Valek",
+    "email": "wvalek3@vk.com",
+    "gender": "Male",
+    "ip_address": "67.76.188.26"
+  }
+]
+
+console.log(test[0].id);
+console.log(test[1].first_name);
+console.log(test[2].last_name);
+console.log(test[3].gender);
